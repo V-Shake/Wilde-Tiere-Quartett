2399f4b036f97d49de79ea3491c1dbfa50daee5f
V-Shake <viviencai6918@gmail.com>
2024-11-09 15:39:20 +0100

    design card layout

--- js/main.js
+++ js/main.js
@@ -8,6 +8,19 @@ $(document).ready(function () {
                 <div class="card-title">${animal.name_german}</div>
                 <div class="card-trivia border-bottom">${animal.trivia_german}
                 </div>
+                <div class="group-rectangle">${animal.group}</div>
+                <div class="groupname">${animal.groupname_german}</div>
+                <div class="intelligence-box">
+                <div class="intelligence-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-bulb">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M3 12h1m8 -9v1m8 8h1m-15.4 -6.4l.7 .7m12.1 -.7l-.7 .7" />
+                <path d="M9 16a5 5 0 1 1 6 0a3.5 3.5 0 0 0 -1 3a2 2 0 0 1 -4 0a3.5 3.5 0 0 0 -1 -3" />
+                <path d="M9.7 17l4.6 0" />
+                </svg>
+                </div>
+                <div class="intelligence-content">${animal.intelligence}</div>
+                </div>
                 <div class="stat-box">
                     <div class="stat-pair">
                         <div class="stat-icon">
@@ -62,3 +75,4 @@ $(document).ready(function () {
         $('#wrapper').append(divBox);
     });
 });
+
